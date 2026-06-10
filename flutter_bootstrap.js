e80c333acaa227d84b448a629d8a61a1e2545b89
Z User <z@container>
2026-06-10 06:15:43 +0000

    Deploy: Edit pop-up modal on all Launch Phase tables - pre-populated with current row values, world-class UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "474770777" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "620037684" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
