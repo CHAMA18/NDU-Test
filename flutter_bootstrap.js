6dd39e38e20ea545c3618002c6c20546f002852f
Z User <z@container>
2026-06-10 05:25:47 +0000

    Deploy: All textfields now have mic/voice input icons - built from latest source (3ec0f4d9)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1246887610" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1369295551" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
